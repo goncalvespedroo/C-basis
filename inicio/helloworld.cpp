19d4178984cfeb3dc074dfbfbc0a3fa9a860f194
goncalvespedroo <goncalvesdiasdasilvaj@gmail.com>
2024-05-04 16:13:21 -0300

    estudo de tipagem,  variaveis e constantes em c++

---
+++ inicio/helloworld.cpp
@@ -0,0 +1,18 @@
+// faremos um hello world. Classico!!
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "HELLO WORLD!!" << endl;
+    cout << "AND EVERYBODY INSIDE IT" << endl;
+    return 0;
+}
+
+/*
+Aqui aprendemos a compilatr, usar o std:: que pode ser substituido por 
+
+using namespace std;
+
+Nesse caso, vamos ter um melhor proveito de tempo, pois,  não se faz mais necessario o uso continuo da tag std.
+
+Tambem, vimos que é necessario adicionar << endl;  para que o comando pule  uma linha assim que terminar a exibição em tela.
+*/
---
+++ variavel.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main(){
+    
+    int x; //declaramos a variavel
+    x = 5; //atribuimos um valor à variavel
+    double y = 6.7; //declaração e atribuição ao mesmo tempo. Foi usada uma tipagem diferente para suportar decimal
+    double soma = x + y;
+
+    cout << x << endl;
+    cout << y << endl;
+    cout << soma << endl;
+
+    return 0;
+
+}
+
+/*
+int = numeros inteiros
+char = um unico caracter
+bool = true ou false
+string = varios caracteres
+double = numeros inteiros e decimais
+void = sem tipo
+*/
